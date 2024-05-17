340f18b6a9426396bd72bd675706678a5648a3c3
JuanBaquero99 <baquerojuan99@gmail.com>
2024-05-17 16:08:36 -0500

    Maquetación Navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,48 +1,82 @@
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+    const activeStyle = "underline"
+    //Objeto para darle estilos y dirección al texto al nuevo elemento
+
     return (
         <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
             <ul className='flex items-center gap-3'>
                 <li className='font-semibold text-lg'>
-                    <NavLink to='/'>
+                    <NavLink //Classname para los estilos y comparativa para determinar si el vinculo esta activo o no, con respectiva redirección
+                        to='/'
+                        className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
                         Bacav
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/all'>
+                    <NavLink
+                        to='/all'
+                        className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
                         All
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/clothes'>
+                    <NavLink
+                        to='/clothes'
+                        className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
                         Clothes
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/others'>
+                    <NavLink to='/others'
+                        className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
                         Others
                     </NavLink>
                 </li>
             </ul>
             <ul className='flex items-center gap-3'>
                 <li>
-                    <NavLink to='/'>
+                    <NavLink
+                        to='/'
+                        className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
                         feid@example.com
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/my-orders'>
+                    <NavLink
+                        to='/my-orders'
+                        className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
                         My Orders
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/my-account'>
+                    <NavLink
+                        to='/my-account'
+                        className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
                         My Account
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/sign-in'>
+                    <NavLink
+                        to='/sign-in'
+                        className={({ isActive }) =>
+                            isActive ? activeStyle : undefined
+                        }>
                         Sign In
                     </NavLink>
                 </li>
